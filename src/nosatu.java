b09f73edf901ffb71eed5c771b8e909a33383cf5
unknown <baiqindri2001@gmail.com>
2022-09-25 11:26:55 +0800

    Tugas Dasar Java

---
+++ src/nosatu.java
@@ -0,0 +1,49 @@
+public class nosatu {
+    public static void main(String[] args) {
+        //Head
+        String bio = "BIODATA SINGKAT";
+        System.out.println(bio);
+
+        //nama lengkap
+        String namaDepan = "Baiq";
+        String namaTengah = "Indri";
+        String namaBelakang = "Astuti";
+        String namaLengkap = namaDepan+" "+namaTengah+" "+namaBelakang;
+        System.out.println("Nama Lengkap : "+namaLengkap);
+
+        //tempat tanggal lahir
+        String tel = "Jorong";
+        int tl = 21;
+        int bl = 04;
+        int tahunl = 2001;
+        System.out.println("TTL : "+tel+"/"+tl+"/"+bl+"/"+tahunl);
+
+        //umur
+        int umura = 1;
+        int umurb = 22;
+        int umur = 1 * 22;
+        System.out.println("Umur : "+umur+" Tahun");
+
+        //jenis kelamin
+        char jk = 'P';
+        System.out.println("Jenis Kelamin : "+jk);
+
+        //alamat
+        String alamat = "Dusun Jororng, Desa Beraim";
+        System.out.println("Alamat : "+alamat);
+        System.out.println();
+
+        //pendidikan
+        String sd = "Sekolah Dasar Negeri Jorong";
+        String smp = "MTS Darul Muttahidin Jorong";
+        String smk = "SMK Al-Hasanain";
+        String kuliah = "STMIK Lombok";
+        System.out.println("Riwayat Pendidikan");
+        System.out.println("Sekolah Dasar : "+sd);
+        System.out.println("Sekolah Menengah Pertama : "+smp);
+        System.out.println("Sekolah Menengah Kejuruan : "+smk);
+        System.out.println("Sekolah Tinggi : "+kuliah);
+
+
+    }
+}
